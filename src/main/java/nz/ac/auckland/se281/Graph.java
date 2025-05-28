5a1a2ac7d20d35969c976af4fb12287d00ff31f7
Caitlin Kuan <caitlinkuan49@gmail.com>
2025-05-29 00:51:46 +1200

    Completing the skeleton of the Graph Class

--- src/main/java/nz/ac/auckland/se281/Graph.java
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,5 +10,25 @@ public class Graph {
 
   public Graph() {
     this.adjNodes = new HashMap<>();
+  }
+
+  public void addNode(Country node) {
+    adjNodes.putIfAbsent(node, new ArrayList<>());
+  }
+
+  public void addEdge(Country node) {
+    addNode(node); // might not need?
+    adjNodes.get(node);
+  }
+
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (Country n : adjNodes.keySet()) {
+      sb.append(n);
+      sb.append("->");
+      sb.append(adjNodes.get(n));
+      sb.append(System.lineSeparator());
+    }
+    return sb.toString();
   }
 }
